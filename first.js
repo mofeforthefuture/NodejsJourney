3a68cb3f6f37706311aa4fc62289e5702f006847
Eyimofe Omotayo <eyimofeomotayo@gmail.com>
2021-08-22 00:29:36 +0100

    imported the module

--- first.js
+++ first.js
@@ -1,8 +1,10 @@
 var http = require('http');
+var dt = require('./myFirstModule')
 
 http.createServer(function (req, res) {
     res.writeHead(200, { 'Content-Type': 'text/html' });
     res.end('Hello Mofe let\'s start learning node js, ok  this is cool')
+    res.write("The date and time are currently: " + dt.myDateTime())
 }).listen(3000)
 
 //To include a module, use the ########require()######## function with the name of the module:
